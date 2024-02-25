f015617679619be1c2ba53dbdf99462c9be4120f
Luis Avendano <LuisAvendanoCastanos@hotmail.com>
2024-02-24 17:11:05 -0700

    Updated intervals, added camera to it's own file

--- hittable.h
+++ hittable.h
@@ -7,11 +7,23 @@ public:
     point3 p;
     vec3 normal;
     double t;
+    bool front_face;
+
+    /// <summary>
+    /// Sets the hit record for normal vector
+    /// </summary>
+    /// <param name="r">Ray to be calculated.</param>
+    /// <param name="outward_normal">Surface normal, assumed to have unit length.</param>
+    void set_face_normal(const ray& r, const vec3& outward_normal) {
+
+        front_face = dot(r.direction(), outward_normal) < 0;
+        normal = front_face ? outward_normal : -outward_normal;
+    }
 };
 
 class hittable {
 public:
     virtual ~hittable() = default;
 
-    virtual bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const = 0;
+    virtual bool hit(const ray& r, interval ray_t, hit_record& rec) const = 0;
 };
